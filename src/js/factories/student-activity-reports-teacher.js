cdadb7458595378728eaabeeb04216cdc9568340
shuvajitmukherjee <shuvajit.mukherjee@learningmate.com>
2016-02-09 14:02:01 +0530

    Factories added

---
+++ src/js/factories/student-activity-reports-teacher.js
@@ -0,0 +1,58 @@
+'use strict'
+var factoryModule = angular.module('studentActivityReports.factories', []);
+
+factoryModule.factory('getData', function($http) {
+
+    var basePath = '/src/js/data/';
+
+    return {
+        _get: function(str) {
+            return $http.get(basePath + str + '.json');
+        }
+    };
+});
+
+
+factoryModule.factory('getEnrollmentStatus', function($http) {
+
+    var service = {};
+
+    service.get = function() {
+        return [
+            {
+                id: 0,
+                name: "Active"
+            },
+            {
+                id: 1,
+                name: "Withdrawn"
+            },
+            {
+                id: 2,
+                name: "WithdrawnFailed"
+            },
+            {
+                id: 3,
+                name: "Transferred"
+            },
+            {
+                id: 4,
+                name: "Completed"
+            },
+            {
+                id: 5,
+                name: "CompletedNoCredit"
+            },
+            {
+                id: 6,
+                name: "Suspended"
+            },
+            {
+                id: 7,
+                name: "Inactive"
+            }
+        ];
+    };
+
+    return service;
+});
